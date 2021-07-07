34b63bb936e04d9d26026de1b37a17d3c79d5842
Shira Goldhaber-Gordon <shirag624@gmail.com>
2021-07-07 14:18:28 -0600

    restoring version with floating sphere

--- 3d-gaussian-graph.py
+++ 3d-gaussian-graph.py
@@ -6,7 +6,7 @@ x=np.linspace(-5,5, num=100)
 y=np.linspace(-5,5, num=100)
 x, y = np.meshgrid(x, y)
  
-z = -np.exp(-0.2*x**2-0.2*y**2)
+z = -np.exp(-0.2*x**2-0.2*y**2)/4
  
 fig = plt.figure()
 ax = fig.add_subplot(111, projection='3d')
@@ -16,12 +16,12 @@ ax.set_axis_off()
  
 #plotting sphere
 u = np.linspace(0, np.pi, 50)
-v = np.linspace(0, 3 * np.pi, 50)
+v = np.linspace(0, 2 * np.pi, 50)
  
 a = np.outer(np.sin(u), np.sin(v))
 b = np.outer(np.sin(u), np.cos(v))
-c = np.outer(0.2*np.cos(u), 0.2*np.ones_like(v))
+c = np.outer(0.4*np.cos(u), 0.4*np.ones_like(v)) + 0.7
  
-ax.plot_surface(a, b, c, cmap="autumn")
+ax.plot_surface(a, b, c)
  
 plt.show()
